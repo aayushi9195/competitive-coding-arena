266acfcda3dc6bdbd05d05f6c521a05fc5802ae9
aayushi9195 <63947723+aayushi9195@users.noreply.github.com>
2025-01-01 21:02:44 +0530

    Update contains_duplicate.py

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -5,7 +5,7 @@ class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         s = set()
         for num in nums:
-            # We found a number already present seen before, so return false
+            # We found a number already seen before, so return false
             if num in s:
                 return True
             # Keep adding numbers to the set as long as we have not seen them before
