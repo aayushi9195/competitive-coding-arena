1a906926070cdcdf11e7139404db8dcc6c3e5aad
aayushi9195 <63947723+aayushi9195@users.noreply.github.com>
2025-01-01 21:02:01 +0530

    Add files via upload

---
+++ contains_duplicate.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    # Time: O(N)
+    # Space: O(N)
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            # We found a number already present seen before, so return false
+            if num in s:
+                return True
+            # Keep adding numbers to the set as long as we have not seen them before
+            s.add(num)
+        return False
+
+# Note: We can achieve the same time/space complexity by shortening the above code to a one-liner:
+# return len(set(nums)) < len(nums)
