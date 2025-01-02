f6f6838ad3e8186b9d1b72754b93cfede10c068c
aayushi9195 <63947723+aayushi9195@users.noreply.github.com>
2025-01-02 11:55:01 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -5,14 +5,12 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
         # number -> index
-        s = {}
-        
+        s = {} 
         for i, n in enumerate(nums):
             if target - n in s:
                 # smaller index needs to be returned first
                 return [s[target - n], i]
             # store the current number and its index until we find the result
             s[n] = i
-        
         # could not find any pair whose sum is target
         return [-1, -1]
