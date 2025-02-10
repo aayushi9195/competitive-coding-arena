c8f3bc01bcc95443a0cec76f6bc3b850b729487a
aayushi9195 <63947723+aayushi9195@users.noreply.github.com>
2025-02-10 10:18:08 +0530

    Create longest_palindromic_substring.py

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,35 @@
+class Solution:
+    # Time: O(N^2)
+    # Space: O(N^2)
+    def longestPalindrome(self, s: str) -> str:
+
+        n = len(s)
+        # If the substring from i to j is a palindrome, dp[i][j] will be True. Set all indices to False initially.
+        dp = [[False] * n for _ in range(n)]
+        start, length = 0, 1
+
+        # All strings with length 1 are palindromes.
+        for i in range(n):
+            dp[i][i] = True
+
+        # Check if strings with length 2 are palindromes or not and update the start index and max length accordingly.
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+                start, length = i, 2
+
+        # For every substring of length 3 to n
+        for k in range(3, n+1):
+            # Consider all possible substrings of length k
+            for i in range(n-k+1):
+                j = i+k-1
+                # Current substring i to j is a palindrome if they outermost characters are same and the inner substring i.e. i+1 to j-1 is also a palindrome.
+                if s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = True
+                    # Update the start index and max length if we find a larger palindrome.
+                    start, length = i, k
+                  
+        # Return the substring which is the longest palindrome in the input string.
+        return s[start : start + length]
+
+# Note: Check Manacher's algorithm for O(N) time and O(N) space. It is a bit complicated to understand and implement.
