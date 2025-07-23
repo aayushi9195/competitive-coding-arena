9a248ce68e5c7bd335c023882d650784072e0c86
aayushi9195 <63947723+aayushi9195@users.noreply.github.com>
2025-07-23 09:37:35 +0530

    Create kth_largest_element_in_array.py

---
+++ leetcode-patterns/top-k-elements/kth_largest_element_in_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    # Time: O(NlogK) as we do heap operations push/pop for every element in the array.
+    # Space: O(K) as we don't allow the heap to grow beyond K elements.
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        import heapq
+        heap = []
+        # Python by default maintains a minimum heap. 
+        # If we limit the size of heap to k - we pop the root i.e. minimum element every time the size of the heap goes beyond k. This means that we will end up having the k largest elements in the heap and the root will be the minimum of those i.e. kth largest element of the array.
+        for num in nums:
+            heapq.heappush(heap, num) # O(logK) time
+            if len(heap) > k:
+                heapq.heappop(heap) # O(logK time)
+        # Return the kth largest element
+        return heap[0]
